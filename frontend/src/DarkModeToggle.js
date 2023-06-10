6de2837c22e81783d9e0abbef09d7afea407f153
JellyFish <kurer456@naver.com>
2023-06-11 00:31:55 +0900

    20230610 first class : vanila js project structure, dark mode setting, refactoring

---
+++ frontend/src/DarkModeToggle.js
@@ -0,0 +1,42 @@
+
+class DarkModeToggle {
+  isDarkMode = null;
+
+  constructor({ $target }) {
+    const $wrapper = document.createElement("section")
+    const $DarkModeToggle = document.createElement("input");
+    this.$DarkModeToggle = $DarkModeToggle;
+    this.$DarkModeToggle.type = "checkbox";
+
+    $DarkModeToggle.className = "DarkModeToggle";
+    $target.appendChild($wrapper);
+    $wrapper.appendChild($DarkModeToggle);
+
+    $DarkModeToggle.addEventListener("change", (e) => {
+      this.setColorMode(e.target.checked)
+    });
+
+    this.initColorMode()
+  }
+
+  initColorMode(){
+    //초기화 : isDarkMode state, checkbox 상태, html attr
+    //현재 브라우저가 다크모드인지 확인
+    this.setState(window.matchMedia("(prefers-color-scheme: dark)").matches)
+    //다크모드 여부를 체크박스에 반영
+    this.$DarkModeToggle.checked = this.isDarkMode;
+    this.setColorMode(this.isDarkMode)
+  }
+
+  setColorMode(isDarkMode) {
+    document.documentElement.setAttribute(
+      "color-mode",
+      isDarkMode ? "dark" : "light"
+    );
+  }
+
+  setState(nextData) {
+    this.isDarkMode = nextData;
+  }
+
+}
--- frontend/src/api.js
+++ frontend/src/api.js
@@ -1,10 +1,11 @@
 const API_ENDPOINT =
-  "https://rhdd0roxs5.execute-api.ap-northeast-2.amazonaws.com/dev";
+  // "https://rhdd0roxs5.execute-api.ap-northeast-2.amazonaws.com/dev"
+  "http://localhost:4001";
 
 const api = {
-  fetchCats: keyword => {
-    return fetch(`${API_ENDPOINT}/api/cats/search?q=${keyword}`).then(res =>
+  fetchCats: (keyword) => {
+    return fetch(`${API_ENDPOINT}/api/cats/search?q=${keyword}`).then((res) =>
       res.json()
     );
-  }
+  },
 };
